520595b7cf30ae96429118779171ae3ec368bfa1
rexhent <dhannah10@outlook.com>
2023-11-12 00:51:44 +1100

    migrate to react router v6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,26 +2,23 @@ import TextEditor from "./TextEditor";
 import DocumentSelect from "./DocumentSelect";
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Redirect,
+  Navigate,
 } from "react-router-dom";
 import { v4 as uuidV4 } from "uuid";
 
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/new/" exact>
-          <Redirect to={`/documents/${uuidV4()}`} />
-        </Route>
-        <Route path="/documents/:id" exact>
-          <TextEditor />
-        </Route>
-        <Route path="/" exact>
-          <DocumentSelect />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route
+          path="/new/"
+          element={<Navigate to={`/documents/${uuidV4()}`} />}
+        />
+        <Route path="/documents/:id" element={<TextEditor />} />
+        <Route path="/" element={<DocumentSelect />} />
+      </Routes>
     </Router>
   );
 }
