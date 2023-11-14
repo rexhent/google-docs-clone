66ce918c74db1e848066f4145623a79a57d35d33
rexhent <dhannah10@outlook.com>
2023-11-14 23:01:03 +1100

    remove js in server

--- server/Document.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const { Scheme, model, Schema } = require("mongoose");
-const DocumentObject = new Schema({
-    _id: String,
-    data: Object,
-});
-module.exports = model("DocumentObject", DocumentObject);
--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,6 @@ const io = require("socket.io")(3001, {
 });
 
 console.log("This server is running");
-console.log(process.env.DB);
 
 const defaultValue = "";
 
