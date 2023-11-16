2e47a9f3cf6e3f75056e5329241e69a102210a2c
rexhent <dhannah10@outlook.com>
2023-11-16 20:00:47 +1100

    add postgres

---
+++ server/documentSchema.js
@@ -0,0 +1,23 @@
+// documentSchema.js
+const { Pool } = require("pg");
+const dotenv = require("dotenv").config();
+
+const pool = new Pool({
+  connectionString: process.env.PG,
+});
+
+async function createDocumentSchema() {
+  const query = `
+    CREATE TABLE IF NOT EXISTS documents (
+      _id VARCHAR PRIMARY KEY,
+      data JSONB
+    );
+  `;
+
+  await pool.query(query);
+}
+
+module.exports = {
+  createDocumentSchema,
+  pool,
+};
---
+++ server/pg.ts
@@ -0,0 +1,62 @@
+const { createDocumentSchema, pool } = require("./documentSchema");
+
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Access-Control-Allow-Origin"],
+    credentials: true,
+  },
+});
+
+console.log("This server is running pg");
+
+const defaultValue: any = {};
+
+io.on("connection", (socket: any) => {
+  socket.on("document-select", async () => {
+    const documents = await getAllDocuments();
+    socket.emit("load-documents", documents);
+  });
+
+  socket.on("get-document", async (documentId: any) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", (delta: any) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data: any) => {
+      await updateDocument(documentId, data);
+    });
+  });
+});
+
+async function findOrCreateDocument(id: any) {
+  if (id == null) return;
+
+  const result = await pool.query("SELECT * FROM documents WHERE _id = $1", [
+    id,
+  ]);
+  const document = result.rows[0];
+
+  if (document) return document;
+
+  await pool.query("INSERT INTO documents (_id, data) VALUES ($1, $2)", [
+    id,
+    defaultValue,
+  ]);
+
+  return { _id: id, data: defaultValue };
+}
+
+async function getAllDocuments() {
+  const result = await pool.query("SELECT _id FROM documents");
+  return result.rows;
+}
+
+async function updateDocument(id: any, data: any) {
+  await pool.query("UPDATE documents SET data = $1 WHERE _id = $2", [data, id]);
+}
