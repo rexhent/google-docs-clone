8b3277aa5c209b3e30f54bcd2d9cb5b22c5326ba
Dominic <67210734+rexhent@users.noreply.github.com>
2023-11-17 13:48:40 +1100

    fix

--- client/src/DocumentSelect.jsx
+++ client/src/DocumentSelect.jsx
@@ -11,9 +11,9 @@ export default function DocumentSelect() {
   const [docName, setDocName] = useState("");
 
   useEffect(() => {
-    const s = io(`http://192.168.150.112:3001`);
+    // const s = io(`http://192.168.150.112:3001`);
     // const s = io(`https://google-docs-clone-backend-r332h.ondigitalocean.app/`);
-    // const s = io(`https://ec2.rexhent.xyz/`);
+    const s = io(`https://ec2.rexhent.xyz/`);
     setSocket(s);
 
     return () => {
