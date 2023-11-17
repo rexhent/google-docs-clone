bb7a298a1b3de1c49f95e1fe29316caaf1c73fe1
rexhent <dhannah10@outlook.com>
2023-11-17 23:18:03 +1100

    icon

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -12,9 +12,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.svg" sizes="any" />
-      </head>
       <body>{children}</body>
     </html>
   );
