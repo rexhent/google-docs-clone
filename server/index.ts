e7a25831258e7fcea8deff2c225711b3cfde9564
rexhent <dhannah10@outlook.com>
2023-12-02 10:35:31 +1100

    oop

--- server/index.ts
+++ server/index.ts
@@ -2,50 +2,56 @@ const mongoose = require("mongoose");
 const DocumentSchema = require("./Document.ts");
 const dotenv = require("dotenv").config();
 
-mongoose.connect(process.env.DB, {});
+class DocumentSocketServer {
+  private io: Server;
+  private defaultValue = "";
 
-const io = require("socket.io")(3001, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Access-Control-Allow-Origin"],
-    credentials: true,
-  },
-});
-
-console.log("This server is running");
-
-const defaultValue = "";
-
-io.on("connection", (socket: any) => {
-  socket.on("document-select", async () => {
-    const documents = await getAllDocuments();
-    socket.emit("load-documents", documents);
-  });
-
-  socket.on("get-document", async (documentId: string) => {
-    const document = await findOrCreateDocument(documentId);
-    socket.join(documentId);
-    socket.emit("load-document", document.data);
-
-    socket.on("send-changes", (delta: Object) => {
-      socket.broadcast.to(documentId).emit("receive-changes", delta);
+  constructor() {
+    mongoose.connect(process.env.MONGO, {});
+    this.io = require("socket.io")(3001, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Access-Control-Allow-Origin"],
+        credentials: true,
+      },
     });
+    this.initializeSocketEvents();
+    console.log("This server is running");
+  }
 
-    socket.on("save-document", async (data: Object) => {
-      await DocumentSchema.findByIdAndUpdate(documentId, { data });
+  private initializeSocketEvents(): void {
+    this.io.on("connection", (socket: any) => {
+      socket.on("document-select", async () => {
+        const documents = await this.getAllDocuments();
+        socket.emit("load-documents", documents);
+      });
+
+      socket.on("get-document", async (documentId: string) => {
+        const document = await this.findOrCreateDocument(documentId);
+        socket.join(documentId);
+        socket.emit("load-document", document.data);
+
+        socket.on("send-changes", (delta: Object) => {
+          socket.broadcast.to(documentId).emit("receive-changes", delta);
+        });
+
+        socket.on("save-document", async (data: Object) => {
+          await DocumentSchema.findByIdAndUpdate(documentId, { data });
+        });
+      });
     });
-  });
-});
+  }
 
-async function findOrCreateDocument(id: string) {
-  if (id == null) return;
+  private async findOrCreateDocument(id: string) {
+    if (id == null) return;
 
-  const document = await DocumentSchema.findById(id);
-  if (document) return document;
-  return await DocumentSchema.create({ _id: id, data: defaultValue });
-}
+    const document = await DocumentSchema.findById(id);
+    if (document) return document;
+    return await DocumentSchema.create({ _id: id, data: this.defaultValue });
+  }
 
-async function getAllDocuments() {
-  return await DocumentSchema.find({}, "_id");
+  private async getAllDocuments() {
+    return await DocumentSchema.find({}, "_id");
+  }
 }
